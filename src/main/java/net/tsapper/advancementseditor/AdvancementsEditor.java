0aac0d9fe66901e7ad44c654ff0a2800177decb8
tsang_3jaxb9w <tsang_3jaxb9w@BLUEBLACKBOX>
2022-10-04 20:51:04 -0500

    Initial commit

---
+++ src/main/java/net/tsapper/advancementseditor/AdvancementsEditor.java
@@ -0,0 +1,16 @@
+package net.tsapper.advancementseditor;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdvancementsEditor implements ModInitializer {
+	public static final String MOD_ID = "advancementseditor";
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/net/tsapper/advancementseditor/AdvancementsEditorClient.java
@@ -0,0 +1,12 @@
+package net.tsapper.advancementseditor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancementsEditorClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
